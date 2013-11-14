ff0caf02a719c78419ca9aa51087da548205e58b
Dezawa <dezawa@aliadne.net>
2013-11-14 22:47:12 +0900

    modified by RAILS4

--- config/routes.rb
+++ config/routes.rb
@@ -1,121 +1,56 @@
-# -*- coding: utf-8 -*-
-ActionController::Routing::Routes.draw do |map|
-  # 全体
-  map.connect '/:controller/:action',:action => /[^0-9]+/
-
-  # UBR
-#  map.connect '/ubr/倉庫状況.pdf'  ,:url => '/public/ubr/倉庫状況.pdf'
-  map.connect '/ubr/:controller/:action'  ,:path_prefix => 'ubr'#,:action => /[^0-9]+/
-
-  # 病院
-  map.connect '/hospital/:controller/:action'  ,:path_prefix => 'hospital'
-  map.connect '/hospital/:controller'  ,:path_prefix => 'hospital'
-
-  map.resources :holyday
-  map.resources :hospital_nurces ,:controller => "hospital/nurces"
-  map.resources :hospital_role ,:controller => "hospital/role"
-  map.resources :hospital_need ,:controller => "hospital/need"
-  map.resources :hospital_meeting,:controller => "hospital/meeting"
-  map.resources :hospital_kinmucode,:controller => "hospital/kinmucode"
-  map.resources :hospital_shokui,:hospital_shokushu,:hospital_kinmukubun
-  map.resources :hospital_limit ,:hospital_busho  , :hospital_monthly
-  map.resources :labels
-
-  # 簿記
-  map.connect '/book/:controller/:action'  ,:path_prefix => 'book'
-  map.connect '/book/:controller'  ,:path_prefix => 'book'
-
-  map.resources :book_main       ,:controller => 'book/main'
-  map.resources :book_permission ,:controller => 'book/permission'
-  map.resources :book_kamoku     ,:controller => 'book/kamoku'
-
-
-  # ん～～～と
-  map.top    '/'      ,:controller => 'top',        :action => 'msdn'
-
-  # ユーザ管理
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.change_password '/change_password', :controller => 'users', :action => 'change_password'
-  map.resources :users
-  map.resources :user_options
-  map.resource :session
-
-  # LiPS
-  map.lips   '/LinierPlanGeneral', :controller => 'top_pages', :action => 'linier_plan_general'
-
-  #map.connect   '/ube_skd/lips_load',:controller => 'ube_skd',:action=>:lips_load
-  map.select    '/ube_skd/select' ,:controller => 'ube_skd', :action => 'select'
-  map.connect   '/ube_skd/input_result',:controller => 'ube_skd',:action=>:input_result
-  map.resources :ube_skd,:ube_plan
-  map.resources :ube_maintain
-  map.resources :ube_meigara
-  map.resources :ube_meigara_shortname
-  map.resources :ube_constant
-  map.connect   '/ube_holyday/edit_on_table'   ,:controller => 'ube_holyday',  :action => "edit_on_table"
-  map.connect   '/ube_holyday/add_on_table'   ,:controller => 'ube_holyday',  :action => "add_on_table"
-  map.connect   '/ube_holyday/update_on_table'   ,:controller => 'ube_holyday',  :action => "update_on_table"
-  map.resources :ube_holyday
-  #map.resources :ubeboard
-  map.resources :ube_product
-  map.resources :ube_operation
-  #map.resources :ube_operations
-  map.resources :Top
-  map.connect   '/ubeboard/:action',:controller => 'ubeboard'
-  map.connect   '/ubeboard/lips_load',:controller => 'ubeboard',:action=>:lips_load
-  map.connect   '/ube_skd/:action',:controller => 'ube_skd'
-  map.connect   '/ube_skd/makeplan',:controller => 'ube_skd',:action=>:makeplan
-  map.connect   '/ube_product/:action',:controller => 'ube_product'
-  map.connect   '/ube_plan/:action',:controller => 'ube_plan'
-  map.connect   '/user_options/:action',:controller => 'user_options'
-  map.connect   '/:controller/csv_upload',:action => "csv_upload"
-  map.resources :ube_named_changes
-#
-#
-
-#
+Msdn4::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
 
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
 
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
   #   end
 
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
 
-  # See how all your routes lay out with "rake routes"
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
 end
