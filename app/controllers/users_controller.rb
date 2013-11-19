7d1a43c3d2de10e365832f1219ed43c727e6f6c6
Dezawa <dezawa@aliadne.net>
2013-11-19 11:54:55 +0900

    表示幅変更とStrong Parameters対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,19 @@ class UsersController < ApplicationController
   
   #Viewにて表示すべき項目の定義。
   Labels0 = [
-             HtmlText.new(:login         ,"ユーザ名",          :size => 30),
-             HtmlText.new(:email         ,"メール",            :size => 30),
-             HtmlPasswd.new(:password    ,"パスワード",        :size => 30),
-             HtmlPasswd.new(:password_confirmation,"確認パスワード",:size => 30),
+             HtmlText.new(:name         ,"氏名",          :size => 20),
+             HtmlText.new(:login         ,"ユーザ名",          :size => 20),
+             HtmlText.new(:email         ,"メール",            :size => 20),
+             HtmlPasswd.new(:password    ,"パスワード",        :size => 20),
+             HtmlPasswd.new(:password_confirmation,"確認パスワード",:size => 20),
              HtmlRadio.new(:lipscsvio    ,"LiPS CSV IO option",
                             :correction => [["可",true],["不可",false]] ),
              HtmlRadio.new(:lipssizeoption    ,"size変更",
                             :correction => [["可",true],["不可",false]] ),
-             HtmlText.new(:lipssizepro   ,"pro数最大",         :size => 30),
-             HtmlText.new(:lipssizeope   ,"ope数最大",         :size => 30),
-             HtmlText.new(:lipslabelcode ,"ラベル",            :size => 30),
-             HtmlText.new(:lipsoptlink   ,"LiPS opt_link",     :size => 30)
+             HtmlText.new(:lipssizepro   ,"pro数最大",         :size => 20),
+             HtmlText.new(:lipssizeope   ,"ope数最大",         :size => 20),
+             HtmlText.new(:lipslabelcode ,"ラベル",            :size => 20),
+             HtmlText.new(:lipsoptlink   ,"LiPS opt_link",     :size => 20)
             ]
 
  def set_instanse_variable
@@ -40,7 +41,6 @@ class UsersController < ApplicationController
     flash[:return_to] = request.env["HTTP_REFERER"]
     @labels = Labels0[2..3]
     @user = User.find(current_user.id)  
-#render :action => :test  
   end
 
   def password_update
@@ -54,8 +54,6 @@ class UsersController < ApplicationController
          render :action => "change_password" 
       end
   end
-
-  # render new.rhtml
 
   def show
     @user = User.find(params[:id],:include=> :user_options)
@@ -72,20 +70,12 @@ class UsersController < ApplicationController
   end
  
   def create
-    #@user = User.new
-    #logout_keeping_session!
     @user = User.new(params[:user])
     success = @user && @user.save
     logger.debug("UserCreate success? #{success}")
     logger.debug("UsetCreate #{@user.errors.on(:email)}")
     if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
-      # protection if visitor resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset session
-      #self.current_user = @user # !! now logged in
-      
-        redirect_to :action => :index #( flash[:return_to] || "/top")#redirect_back_or_default('/top')
+        redirect_to :action => :index
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
@@ -97,12 +87,11 @@ class UsersController < ApplicationController
   def update
     @params = params
     @user = User.find(params[:id],:include => :user_options)
-    @user[:valtype] = @user[:lips_size_pro].class
+    #@user[:valtype] = @user[:lips_size_pro].class
     @user_option_ids = params[:user_options].map{|id,ok_ng| id if ok_ng == "1" }.compact
     @user.user_options = UserOption.find(@user_option_ids)
-    #render :action => :test ; return
     respond_to do |format|
-      if @user.update_attributes(params[:user])
+      if @user.update_attributes(permit_attr)
         flash[:notice] = 'User was successfully updated.'
         format.html { redirect_to(@user) }
         format.xml  { head :ok }
@@ -114,5 +103,6 @@ class UsersController < ApplicationController
     end
 
   end
+
 
 end # of class users_cont
