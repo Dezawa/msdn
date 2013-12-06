37676281f9840bff762141eade7facd4ef38c87c
Dezawa <dezawa@aliadne.net>
2013-12-06 20:06:59 +0900

    validationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,16 +19,13 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :user_options
 
-  #validates_presence_of     :login
-  #validates_length_of       :login,    :within => 3..40
-  #validates_uniqueness_of   :login
-  #validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
-
-  #validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
+  validates_presence_of     :username
+  validates_length_of       :username,    :within => 3..40
+  validates_uniqueness_of   :username
   validates_length_of       :name,     :maximum => 100
 
   validates_presence_of     :email
-  validates_length_of       :email,    :within => 6..100 #r@a.wk
+  #validates_length_of       :email,    :within => 6..100 #r@a.wk
   #validates_uniqueness_of   :email
   #validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
   validates_presence_of     :password,                   :if => :password_required?
@@ -62,6 +59,7 @@ class User < ActiveRecord::Base
   end
 
   def login ; username ;end
+
   def login=(value)
     write_attribute :username, (value ? value.downcase : nil)
   end
