6312312ac82dc8ff378c8ff2f5726cda857e0616
Dezawa <dezawa@aliadne.net>
2013-12-08 12:06:16 +0900

    RAILS_ROOTからRails.rootに変更、ActionController::UploadedTempfileからActionDispatch::Http::UploadedFileに変更, MenuCsvを探せないことがあるので、その前に　Menuを呼ぶ

--- app/controllers/ubr/main_controller.rb
+++ app/controllers/ubr/main_controller.rb
@@ -3,7 +3,7 @@ class Ubr::MainController < ApplicationController
   include ExcelToCsv
   before_filter :set_instanse_variable
 
-
+   dmy=Menu
    Labels = [MenuCsv.new("SCM在庫更新" ,"ubr/main"   ,:enable_csv_upload=> true,:size => 40,:buttonlabel=>"CSV/Excelで登録") ,
              Menu.new(   "在庫表示"    ,:main   ,:action => :occupy_pdf) ,
              Menu.new(   "再計算"      ,:main   ,:action => :reculc) ,
@@ -18,7 +18,7 @@ class Ubr::MainController < ApplicationController
     @Links=Labels
 
     @filename = "枠詰まり具合"
-    @pdffile  =File.join(RAILS_ROOT,"tmp","ubr",@filename)
+    @pdffile  =File.join(Rails.root,"tmp","ubr",@filename)
   end
 
   # メニューを出す
@@ -52,14 +52,15 @@ class Ubr::MainController < ApplicationController
   end
 
   def csv_upload
+pp params[:csvfile].class
     @filename = case infile = params[:csvfile]
-                when ActionController::UploadedTempfile; infile.original_filename
+                when ActionDispatch::Http::UploadedFile; infile.original_filename
                 end
     @basename = @filename.sub(/\....$/,"")
     csvfiles = csv_files(params[:csvfile])
     open(Ubr::Lot::SCMFILE,"w"){ |fp| fp.write(File.read(csvfiles[0]))}
     open(Ubr::Lot::SCMFILEBASE,"w"){ |fp| fp.puts(@filename) }
-    open(File.join(RAILS_ROOT,"tmp","ubr","save",@basename+".csv"),"w"){
+    open(File.join(Rails.root,"tmp","ubr","save",@basename+".csv"),"w"){
       |fp| fp.write(File.read(csvfiles[0]))}
     logger.debug("在庫更新 Lotlist数 #{Ubr::LotList.lotlist.list.size}")
     
@@ -67,7 +68,7 @@ class Ubr::MainController < ApplicationController
     
     save_ps
 
-    File.join(RAILS_ROOT,"tmp","ubr","pssave",@basename+".csv")
+    File.join(Rails.root,"tmp","ubr","pssave",@basename+".csv")
 
     flash[:message] = "更新成功:" + @filename
     redirect_to :action => :reculc 
@@ -75,7 +76,7 @@ class Ubr::MainController < ApplicationController
 
   def save_ps
     date = /\d{8}/.match(@basename)
-    psgz = File.join(RAILS_ROOT,"tmp","ubr","pssave","plan")+"-#{date}.ps.gz"
+    psgz = File.join(Rails.root,"tmp","ubr","pssave","plan")+"-#{date}.ps.gz"
     `/bin/gzip -c #{@pdffile+".ps"} > #{psgz}`
   end
 
@@ -87,6 +88,6 @@ class Ubr::MainController < ApplicationController
   end
 
   def show_pdf
-    send_file RAILS_ROOT+"/tmp/ubr/SoukoInfo.gif", :type => 'image/gif', :disposition => 'inline'
+    send_file Rails.root+"/tmp/ubr/SoukoInfo.gif", :type => 'image/gif', :disposition => 'inline'
   end
 end
