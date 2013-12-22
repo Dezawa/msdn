e0b2476b757744edd47625f724d14e9dddd25802
Dezawa <dezawa@aliadne.net>
2013-12-22 18:23:13 +0900

    権限確認をmethodにし、free,memberをやめてcalsにまとめた

--- app/controllers/lips_controller.rb
+++ app/controllers/lips_controller.rb
@@ -4,9 +4,12 @@ class LipsController < ApplicationController
   include LipsHelper
   include CsvIo
   #before_filter :login_required , :except => [:free,:calc,:error]
-  before_filter {|ctrl| ctrl.set_permit %w(LiPS会員)}
-  before_filter(:only => [:csv_upload,:csv_download,:change_form]){|ctrl| ctrl.require_permit "/msg_lips_login"} 
-  skip_before_filter :verify_authenticity_token
+  before_action :authenticate_user! ,except: [:msdn,:free,:calc]
+  before_filter {|ctrl| ctrl.set_permit %w(LiPS会員 LiPS会員 LiPS会員)}
+  before_filter(:only => [:csv_upload,:csv_download,:change_form]){|ctrl|
+    ctrl.require_permit "/msg_lips_login"
+  } 
+  #skip_before_filter :verify_authenticity_token
   max= "3"
   Permit = ["promax", "opemax", "vertical", "minmax", 
             "opename"=>("1"..max).to_a ,
@@ -20,36 +23,28 @@ class LipsController < ApplicationController
 #Hash[*("1"..max).map{|i| [i,("1"..max).to_a]}.flatten(1)]
            ]
 
-
-  def free
-    @title = "線形計画法"
-    I18n.locale = :guest
-    #self.current_user = User.find_by_login("guest")
-    redirect_to :action => :calc
-  end
-
-
-  def member
-    @title = "線形計画法"
-    @user  = current_user ? current_user : User.find_by_login("guest")
-    #I18n.locale = (@login == "guest") ? :guest : :default #user.lipslabelcode
-    I18n.locale = @permit ? :default : :guest   #user.lipslabelcode
-    redirect_to :action => :calc
+  def is_member?
+    @user  = current_user ? current_user : User.find_by(username: "guest")
+    if @permit
+      I18n.locale = :default
+    else
+      I18n.locale = :guest
+    end
   end
 
   def ube_hospital
     @title = "線形計画法"
     I18n.locale = :hospital
-   @user  = current_user ? current_user : User.find_by_login("guest")
+   @user  = current_user ? current_user : User.find_by(username: "guest")
    redirect_to :action => :calc
   end
 
 
   def calc
     @title = "線形計画法"
-   # begin
-      @user  = current_user ? current_user : User.find_by(login: "guest")
-    @login = @user ? @user.login : "guest"
+    is_member?
+    #@user  = current_user ? current_user : User.find_by(username: "guest")
+    @login = @user ? @user.username : "guest"
     set_filename
     if params[:lips]
       @lips=Lips.new(params.require(:lips).permit!)#(Permit))#!)
@@ -62,14 +57,14 @@ class LipsController < ApplicationController
 
 
   def csv_download
-    filename = current_user.login + "_LiPS.csv"
+    filename = current_user.username + "_LiPS.csv"
     send_file(params[:file_name], :filename => filename)    
   end
 
   def csv_upload
     @title = "線形計画法"
     param =HashWithIndifferentAccess.new
-    @user  = current_user ? current_user : User.find_by_login("guest")
+    @user  = current_user ? current_user : User.find_by(username: "guest")
     param[:promax]=current_user.lipssizepro
     param[:opemax]=current_user.lipssizeope	
     @lips = Lips.new(param)
@@ -86,8 +81,8 @@ class LipsController < ApplicationController
 
   def change_form
     @title = "線形計画法"
-      @user  = current_user ? current_user : User.find_by_login("guest")
-    @login = @user ? @user.login : "guest" 
+      @user  = current_user ? current_user : User.find_by(username: "guest")
+    @login = @user ? @user.username : "guest" 
     @lips=Lips.new(params[:lips])
     set_filename
     render :action => :calc
@@ -95,7 +90,7 @@ class LipsController < ApplicationController
 
 protected
   def set_filename
-    user = @user ? @user.login : "guest" 
+    user = @user ? @user.username : "guest" 
     @filebase ="#{user}-#{Time.now.to_i.to_s(16)}"
     @csvfile = "lips/tmp/#{@filebase}.csv"
     @prefix = Rails.root+"public"
