0e1b9ce643fff6d7950b2daeb3346f9a5c05e274
Dezawa <dezawa@aliadne.net>
2014-01-05 13:05:40 +0900

    とりあえず

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214002521) do
+ActiveRecord::Schema.define(version: 20131227060249) do
 
   create_table "book_kamokus", force: true do |t|
     t.text    "kamoku"
@@ -252,6 +252,7 @@ ActiveRecord::Schema.define(version: 20131214002521) do
     t.string   "state",                              default: "passive"
     t.datetime "deleted_at"
     t.string   "subdomain"
+    t.datetime "confirmed_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
