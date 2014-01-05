12f65e8c07fcd46fef97c7d6d7ec20377513f509
Dezawa <dezawa@aliadne.net>
2014-01-05 19:44:37 +0900

    とりあえず

--- spec/controllers/book/main_controller_spec.rb
+++ spec/controllers/book/main_controller_spec.rb
@@ -79,7 +79,7 @@ describe Book::MainController,"一覧画面にて on 2012年" do
   it "1件めID=1 の削除" do
     expect{delete "destroy", id: 1}.to change{Book::Main.count}.
       from(106).to(105)
-    expect(response).to redirect("index")
+    expect(response).to redirect_to("/book/main")
   end
 
   
