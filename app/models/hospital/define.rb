1615a62b64b2e15e754ea9a4965d332750530324
Dezawa <dezawa@aliadne.net>
2014-01-17 18:23:36 +0900

    allから変更

--- app/models/hospital/define.rb
+++ app/models/hospital/define.rb
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 class Hospital::Define < ActiveRecord::Base
-  set_table_name 'hospital_defines'
+  self.table_name = 'hospital_defines'
 
   def self.koutai3?
     #define=Hospital::Define.find_by_attribute("hospital_Koutai")
-    define=Hospital::Define.all(:conditions => ["attri = ?","hospital_Koutai"])[0]
+    define=Hospital::Define.find_by( attri: "hospital_Koutai")
     !!(define && define.value == "三交代")      
   end
   
