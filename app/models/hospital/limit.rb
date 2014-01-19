df7f8daea01a9aaeb74f6f080b2405897f7da662
Dezawa <dezawa@aliadne.net>
2014-01-19 19:40:08 +0900

    belong と hasの関係を逆にしていた

--- app/models/hospital/limit.rb
+++ app/models/hospital/limit.rb
@@ -1,7 +1,7 @@
 class Hospital::Limit < ActiveRecord::Base
   extend CsvIo
   
-  belongs_to :nurce , :class_name => "Hospital::Nurce"
+  has_one :nurce , :class_name => "Hospital::Nurce"
 
   self.table_name = 'hospital_limits'
   Code = [:code0,:code1,:code2,:code3,:coden]
