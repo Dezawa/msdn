5c45eab1509f84af4957f99d7cdeded0ea3ec87c
Dezawa <dezawa@aliadne.net>
2014-01-19 19:36:31 +0900

    項目symbolのtypo

--- app/controllers/hospital/kinmucodes_controller.rb
+++ app/controllers/hospital/kinmucodes_controller.rb
@@ -7,6 +7,7 @@ class Hospital::KinmucodesController < Hospital::Controller
                           :correction =>  Hospital::Const::Kinmukubun),
            HtmlText.new(:code            ,"Code"      ,:size =>3),
            HtmlText.new(:name            ,"名称"                 ),
+#           HtmlText.new(:shift           ,"シフト"        ,:size =>3),
            HtmlText.new(:color           ,"色"        ,:size =>3),
            HtmlText.new(:start           ,"開始"          ,:size =>3),
            HtmlText.new(:finish          ,"終了"          ,:size =>3),
@@ -16,8 +17,8 @@ class Hospital::KinmucodesController < Hospital::Controller
            HtmlText.new(:pm              ,"午後"          ,:size =>3),
            HtmlText.new(:night           ,"準夜"          ,:size =>3),
            HtmlText.new(:midnight        ,"深夜"          ,:size =>3),
-           HtmlText.new(:am              ,"前応"      ,:size =>3),
-           HtmlText.new(:pm              ,"後応"      ,:size =>3),
+           HtmlText.new(:am2             ,"前応"      ,:size =>3),
+           HtmlText.new(:pm2             ,"後応"      ,:size =>3),
            HtmlText.new(:night2          ,"準応"      ,:size =>3),
            HtmlText.new(:midnight2       ,"深応"      ,:size =>3),
            HtmlText.new(:main_daytime    ,"日勤"  ,:size =>3),
@@ -39,4 +40,5 @@ class Hospital::KinmucodesController < Hospital::Controller
     @labels= Labels
     super
   end
+
 end
