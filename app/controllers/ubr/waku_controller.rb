fdaf33353ee65c527bb6560dfa5027553c2b5317
Dezawa Shinichiro <dezawa@aliadne.net>
2014-02-22 22:34:09 +0900

    Ube::WakuのAR化

---
+++ app/controllers/ubr/waku_controller.rb
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+class Ubr::WakuController <  Ubr::Controller
+  include ExcelToCsv
+  before_filter :login_required 
+
+
+   Labels = [HtmlText.new(:name,"枠名",:size =>7),
+             HtmlSelect.new(:palette  ,"枠サイズ",:correction =>%w(N 14 11 S)),
+             HtmlText.new(:dan3,"3段枡数",:align=>:right,:size => 3),
+             HtmlText.new(:dan2,"2段枡数",:align=>:right,:size => 3),
+             HtmlText.new(:dan1,"1段枡数",:align=>:right,:size => 3),
+             HtmlText.new(:retusu,"列数",:align=>:right,:size => 3),
+             HtmlSelect.new(:direct_to,"方向"    ,:correction =>%w(→ ← ↑ ↓)),
+             HtmlText.new(:pos_x ,"X位置",:align=>:right,:size => 5),
+             HtmlText.new(:pos_y ,"Y位置",:align=>:right,:size => 5)
+            ]
+
+  def set_instanse_variable
+    @labels= Labels
+    @Model = Ubr::Waku
+    @TYTLE = "UBR：枠"
+    @Links = Links
+    @Domain= @Model.name.underscore
+    @SortBy    = :name
+    @Delete = @editor
+    @Edit =   @editor
+    @TableEdit  =  [[:add_buttom,:dmy,:dmy],[:form,:edit_on_table,"編集"],
+                    [:form,:csv_out,"CSVダウンロード"],
+                    [:csv_up_buttom,:dmy,:dmy]] 
+  end
+
+  def index
+    if params[:prefix]
+      @FindOption = {:conditions => "name like '#{params[:prefix]}%'" } #,params[:prefix] ]    }
+    end
+    super
+  end
+
+
+end
