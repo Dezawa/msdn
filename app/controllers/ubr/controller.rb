f532d5fcc32b1a029b82cfbb3488c882ae81bb98
Dezawa Shinichiro <dezawa@aliadne.net>
2014-02-22 22:33:16 +0900

    UBRの親コントローラーを作る

---
+++ app/controllers/ubr/controller.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+class Ubr::Controller < ApplicationController
+  before_filter {|ctrl| ctrl.set_permit %w(UBR UBRメンテ UBRメンテ)}
+  before_filter :set_instanse_variable
+   Links = [MenuCsv.new("SCM在庫更新" ,"ubr/main"   ,:enable_csv_upload=> true,:size => 40,:buttonlabel=>"CSV/Excelで登録") ,
+             Menu.new(   "在庫表示"    ,:main   ,:action => :occupy_pdf) ,
+             Menu.new(   "再計算"      ,:main   ,:action => :reculc) ,
+             Menu.new(   "枠管理"      ,:waku   ,:action => :index,:disable => :editor) 
+            ]
+end
