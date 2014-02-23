0bcbd54f6938bf703c366d534b25447c877fe43d
Dezawa Shinichiro <dezawa@aliadne.net>
2014-02-23 10:59:54 +0900

    waku AR化に対応

--- test/unit/ubr/lot_test.rb
+++ test/unit/ubr/lot_test.rb
@@ -2,18 +2,19 @@
 require 'test_helper'
 
 class Ubr::LotTest < ActiveSupport::TestCase
+  fixtures :ubr_wakus
   # Replace this with your real tests.
   def setup
-      @Waku    = Ubr::Waku.waku(true) #load_from_master
-      @lotlist = Ubr::LotList.lotlist(true).list
+      @Waku    = Ubr::Waku.all#waku(true) #load_from_master
+      @lotlist = Ubr::LotList.lotlist(true,:file => File.join(RAILS_ROOT,"test","testdata","SCM在庫一覧.csv")).list
   end
 
   must "枠数" do
-    assert_equal 2091,@Waku.size
+    assert_equal 2090,@Waku.size
   end
 
   must "ロット数" do
-    assert_equal 1626,@lotlist.size
+    assert_equal 1624,@lotlist.size
   end
 
   #[ ["G123Z481--------F7","14",3],["G1230540P-------F4","11",2],["G123Z670--------F7","14",1]].
@@ -24,10 +25,10 @@ class Ubr::LotTest < ActiveSupport::TestCase
   #  end
   #}
 
-  must "G123V322--C17---N3 G3097D の枠" do
-    lot = findlot_by_lot("G3097D").first
-    assert_equal ["1A3D"],lot[1].waku.map(&:name)
-    assert_equal ["G3097D"],lot[1].waku.first.lot_list.map(&:lot_no)
+  must "G123V322--C17---N3 G3305D の枠" do
+    lot = findlot_by_lot("G3305D").first
+    assert_equal ["2E3G"],lot[1].waku.map(&:name)
+    assert_equal ["63157","G3305D"],lot[1].waku.first.lot_list.map(&:lot_no)
   end
 
 # G123F222--------F7
