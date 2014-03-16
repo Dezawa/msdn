b901f9625ad01930c7ec3d6a0b0e727aa13c2017
Dezawa Shinichiro <dezawa@aliadne.net>
2014-03-16 17:46:00 +0900

    floorと枠ブロックの関連

--- test/unit/ubr/souko_floor_test.rb
+++ test/unit/ubr/souko_floor_test.rb
@@ -13,6 +13,14 @@ class Ubr::SoukoFloorTest < ActiveSupport::TestCase
     assert_equal ["1A1", "1A2", "1A3", "1A4", "1B2", "1B3", "1B4", "1B5", "1B5", "1B5", "1B6", "1b"],
     @floor.contents
     assert_equal 12,@floor.contents.size
+    assert_equal ["A", "A", "A", "A", "A", "A", "A", "Z", "B", "N", "A", "1"],@floor.sufix
+    assert_equal %w( Z Z Z Z Z Z Z Z M R Z 6),@floor.max
+    assert_equal [[0.0, 0.0], [20.0, -0.2], [20.0, 2.4], [8.0, 2.4],
+                  [15.0, -0.2], [15.0, 4.0], [15.0, -1.4], [14.0, 1.2],
+                  [14.0, 1.2], [-3.0, -2.0], [14.0, -0.2], [0.0, 0.0]],@floor.label_pos,"label_pos"
+    assert_equal [[0.0, 0.0], [4.7, 33.9], [7.6, 28.3], [36.2, 28.3],
+                  [61.2, 39.9], [61.2, 32.0], [56.3, 23.5], [56.3, 16.0],
+                   [86.0, 18.0],[56.3, 16.0], [56.3, 5.0], [56.0, 5.0]],@floor.base_points,"base_point"
     assert_equal  [[0,0],[90,46.5]],@floor.outline
   end
 
