2cf17048f6b1ab5a3e47e36b805a0884b3552d85
Dezawa Shinichiro <dezawa@aliadne.net>
2014-03-21 15:26:49 +0900

    indexのラベルを2段にした。その関係でCSVlabelも修正

--- app/controllers/ubr/pillar_controller.rb
+++ app/controllers/ubr/pillar_controller.rb
@@ -9,18 +9,18 @@ class Ubr::PillarController <  Ubr::Controller
                             ),
              HtmlText.new(:kazu_x,"横本数",:align=>:right,:size => 3),
              HtmlText.new(:kazu_y,"縦本数",:align=>:right,:size => 3),
-             HtmlText.new(:start_x,"開始位置X",:size => 3),
-             HtmlText.new(:start_y,"開始位置Y",:align=>:right,:size => 3),
-             HtmlText.new(:kankaku_x,"横間隔",:align=>:right,:size => 3),
-             HtmlText.new(:kankaku_y,"縦間隔",:align=>:right,:size => 3),
-             HtmlText.new(:size_x,"柱横幅",:align=>:right,:size => 3),
-             HtmlText.new(:size_y,"柱縦幅",:align=>:right,:size => 3)
+             HtmlText.new(:start_x,"X",:size => 3),
+             HtmlText.new(:start_y,"Y",:align=>:right,:size => 3),
+             HtmlText.new(:kankaku_x,"横",:align=>:right,:size => 3),
+             HtmlText.new(:kankaku_y,"縦",:align=>:right,:size => 3),
+             HtmlText.new(:size_x,"横",:align=>:right,:size => 3),
+             HtmlText.new(:size_y,"縦",:align=>:right,:size => 3)
             ]
 
   def set_instanse_variable
     @labels= Labels
     #@AssosiationLabels= FloorLabels
-    #@TableHeaderMulti = [3,[2,"描画原点"],[4,"集計書き出し"],1]
+    @TableHeaderMulti = [4,[2,"開始位置"],[2,"間隔"],[2,"柱幅"]]
     @Model = Ubr::Pillar
     #@ThroughTable = Ubr::SoukoFloorSoukoPlan
     #@assosiation = :souko_floor_souko_plans
@@ -36,7 +36,7 @@ class Ubr::PillarController <  Ubr::Controller
                     [:form,:csv_out,"CSVダウンロード"],
                     [:csv_up_buttom,:dmy,:dmy]]  
     @CSVatrs = Labels.map{|lbl| lbl.symbol}
-    @CSVlabels= Labels.map{|lbl| lbl.label}
+    @CSVlabels= Labels.map{|lbl| lbl.symbol}
     super
   end
 
