351d08e7dd75c7ce47d76cf6f748bd4c91fc277a
Dezawa Shinichiro <dezawa@aliadne.net>
2014-03-23 22:20:57 +0900

    不要コード削除

--- app/models/ubr/souko_floor_souko_plan.rb
+++ app/models/ubr/souko_floor_souko_plan.rb
@@ -22,31 +22,8 @@ module Ubr
     belongs_to :souko_floor,:class_name => "Ubr::SoukoFloor"
     belongs_to :souko_plan,:class_name => "Ubr::SoukoPlan"
 
-  delegate :logger, :to=>"ActiveRecord::Base"
-  #attr_reader :name, :floor_offset, :offset,:floor,:stat_names,:stat_reg , :stat_offset, :stat_font
+    delegate :logger, :to=>"ActiveRecord::Base"
 
     def offset; [offset_x,offset_y] ;end
-    def stat_offset; [stat_offset_x,stat_offset_y] ;end
-    def stat_names ; stat_name_list.split ;end
-    def stat_reg   ; stat_reg_list.split.map{ |r| %r(#{r})} ;end
-    @@SoukoPlan = [] # ]Hash.new{ |h,k| h[k]=[]}
-
-    def self.plans
-      return @@SoukoPlan if @@SoukoPlan.size >0 
-      #SP.each{ |souko_group| @@SoukoPlan [souko_group[:name]] = self.new(souko_group) }
-      Ubr::Const::SP.each{ |souko_group| 
-        @@SoukoPlan << [souko_group[:name], self.new(souko_group),souko_group[:landscape]] 
-      }
-      @@SoukoPlan
-    end
-
-
-    def dinitialize(args={ })
-      [:name ,:sufix,:floor,:floor_offset    , :offset,
-       :stat_names,:stat_reg , :stat_offset , :stat_font
-      ].each{| attr_name| 
-        instance_variable_set  "@#{attr_name}",args.delete(attr_name) 
-      }
-    end
   end
 end
