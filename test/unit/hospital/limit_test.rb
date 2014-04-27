1d1cb6d4dc034b2389c9bd41a8a1d05c52c7eb97
Dezawa Shinichiro <dezawa@aliadne.net>
2014-04-27 09:44:04 +0900

    休日データを取り込むようにしないとneedsが正しく出ない

--- test/unit/hospital/limit_test.rb
+++ test/unit/hospital/limit_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 
 class Hospital::RoleTest < ActiveSupport::TestCase
   fixtures :hospital_needs,:hospital_roles,:nurces_roles,:nurces,:hospital_defines
-  fixtures :bushos,:hospital_limits
+  fixtures :bushos,:hospital_limits,:holydays
 
   def setup
      $HP_DEF = Hospital::Define.create
-    @month     = Time.local(2014,3,1)
+    @month     = Time.parse("2014/3/1 JST")
     # 休日 11 平日 20  20*9+11*6=180+66 = 246
     #                  4*31 = 124         124 = 370
   end
@@ -17,11 +17,11 @@ class Hospital::RoleTest < ActiveSupport::TestCase
     assert_equal %w(2 3),$HP_DEF.night
     assert_equal ({
                     [ 3, "kinmu_total"]=> 62,[ 3, "night_total"]=>62,  
-                    [ 4, "kinmu_total"]=>373,[ 4, "night_total"]=>124,  
+                    [ 4, "kinmu_total"]=>370,[ 4, "night_total"]=>124,  
                     [ 9, "kinmu_total"]=> 93,[ 9, "night_total"]=>62,      
                     [10, "kinmu_total"]=> 93,[10, "night_total"]=>62,     
                     [ 3, "2"]=>31,  [ 3, "3"]=>31,
-                    [ 4, "3"]=>62,  [ 4, "2"]=>62, [ 4, "1"]=>249, [4, "0"]=>0,
+                    [ 4, "3"]=>62,  [ 4, "2"]=>62, [ 4, "1"]=>246, [4, "0"]=>0,
                     [ 9, "3"]=>31,  [ 9, "2"]=>31, [ 9, "1"]=> 31,
                     [10, "3"]=>31,  [10, "2"]=>31, [10, "1"]=> 31,
                   }),Hospital::Limit.need_roles(1,@month)
@@ -41,11 +41,11 @@ class Hospital::RoleTest < ActiveSupport::TestCase
 
   margin = { 
     [3, "kinmu_total"]=>158,  [3, "night_total"]=> 28,  
-    [4, "kinmu_total"]=> 45,  [4, "night_total"]=> 32,
+    [4, "kinmu_total"]=> 48,  [4, "night_total"]=> 32,
     [9, "kinmu_total"]=>127,  [9, "night_total"]=> 23,
     [10, "kinmu_total"]=>171, [10, "night_total"]=>41,
     [3, "2"]=> 19,  [3, "3"]=> 19,
-    [4, "1"]=>131,  [4, "2"]=>24, [4, "3"]=>24,
+    [4, "1"]=>134,  [4, "2"]=>24, [4, "3"]=>24,
     [9, "1"]=> 169, [9, "2"]=> 16,  [9, "3"]=>16,
     [10, "1"]=>209, [10, "2"]=>26, [10, "3"]=>26,
   }
@@ -73,10 +73,15 @@ class Hospital::RoleTest < ActiveSupport::TestCase
     assert_equal -1,margin_roles[[3,"night_total"]]
 #pp margin_roles
     hospital_limit = Hospital::Limit.enough?(1,@month)
-    assert_equal 3, hospital_limit[0].size
-    assert_equal "夜勤計にはリーダーが延べ 62人日必要なところ、1人日不足のため、計算不能です",hospital_limit[0][0]
-    assert_equal "勤務計には看護師が延べ 370人日必要なところ余裕は3人日です。計算時間が掛かるかもしれません",hospital_limit[0][1]
-    assert_equal "夜勤計には看護師が延べ 124人日必要なところ余裕は3人日です。計算時間が掛かるかもしれません",hospital_limit[0][2]
+    assert_equal 2, hospital_limit[0].size
+    warn = hospital_limit[0].sort
+    ret  = ["夜勤計にはリーダーが延べ 62人日必要なところ、1人日不足のため、計算不能です",
+            "夜勤計には看護師が延べ 124人日必要なところ余裕は3人日です。計算時間が掛かるかもしれません"
+           ].sort
+    assert_equal ret,warn,"警告"
+    assert_equal ret[0],warn[0],"警告０"
+    assert_equal ret[1],warn[1],"警告１"
+    assert_equal ret[2],warn[2],"警告２"
 
   end
 
