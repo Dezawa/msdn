cc0058a382ee4a4adfaa0f7c0a7ea208588eb7ed
Dezawa Shinichiro <dezawa@aliadne.net>
2014-05-06 13:46:59 +0900

    リーダーのIDも保持

--- app/models/hospital/define.rb
+++ app/models/hospital/define.rb
@@ -7,7 +7,7 @@ class Hospital::Define < ActiveRecord::Base
 
   attr_reader *Hospital::Define.all.map{ |define| define.attri.to_sym}
   attr_reader :koutai3,:shifts_int,:shifts ,:shifts123,:shiftsmx , :night , :shifts_night 
-  attr_accessor :kangoshi
+  attr_accessor :kangoshi,:leader
   def self.koutai3?
     #define=Hospital::Define.find_by_attribute("hospital_Koutai")
     define=Hospital::Define.all(:conditions => ["attri = ?","hospital_Koutai"])[0]
@@ -24,6 +24,7 @@ class Hospital::Define < ActiveRecord::Base
     self.class.all.each{ |df| instance_variable_set("@#{df.attri}",df.value)}
     @koutai3  = (@hospital_Koutai == "三交代") 
     @kangoshi = Hospital::Role.find_by_name("看護師").id
+    @leader    = Hospital::Role.find_by_name("リーダー").id
     @shifts_int= @koutai3 ? Shift0123 : Shift0123[0..-2]
     @shifts = @koutai3    ? Sshift0123 : Sshift0123[0..-2]
     @shifts123 = @koutai3 ? Sshift123  : Sshift123[0..-2]
