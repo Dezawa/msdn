ff0b138d7bfb17adaf2bcf2fd8733298c9806517
Dezawa Shinichiro <dezawa@aliadne.net>
2014-06-26 08:24:28 +0900

    正規化消費電力のYレンジ変更

--- app/models/shimada/gnuplot_def.rb
+++ app/models/shimada/gnuplot_def.rb
@@ -47,9 +47,11 @@ set out 'tmp/shimada/power.gif'
 
 set title "正規化消費電力 " 
 %s
-set yrange [0:1.1]
+set yrange [0.2:1.1]
 set xrange [1:24]
 set xtics 1,1
+set x2tics 3,3
+set grid x2tics
 !
 
   ########## ↑ GNUPLOT ############
