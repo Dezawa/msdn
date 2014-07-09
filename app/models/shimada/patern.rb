57e22b4ecf4d5dec793fb18b0a03fb2ae095515a
Dezawa Shinichiro <dezawa@aliadne.net>
2014-07-10 07:17:45 +0900

    標準型への分類ほぼ終わり。中に混じるものの分離を残す

--- app/models/shimada/patern.rb
+++ app/models/shimada/patern.rb
@@ -2,16 +2,22 @@
 module Shimada::Patern
   #module ClassStatic
 
-  Paterns = {
-    "稼働無" => %w(0S 1S)        ,"稼働4一時低下" => %w(4H)    ,"稼働4" => %w(400 4F),"稼働4→3" => %w(4-- 4-+ 4-0 4d),
-    "稼働3" => %w(3-- 30- 3F 300 3O),"稼働3一時低下" => %w(3H)    ,"稼働3→2" => %w(3d 3-0),
+  Paterns = {   "稼働無" => %w(0S 1S) , "稼働2" => %w(200 2O 2--F),
+    "稼働3" => %w(3-- 3--F 30- 3F 300 3O),"稼働4" => %w(400 4O 4F 4--F 4+-),
+    "稼働3→2" => %w(3--D 3-+ 3-0)   ,"稼働4→3" =>  %w(4--D 4-+ 4-0),
+    "稼働4一時低下" => %w(40+)    ,"稼働3一時低下" => %w(30+)    ,
+    "未分類" => nil
+   }
+{ 
+    "稼働4→3" => %w(4-- 4-+ 4-0 4d),
+   "稼働3→2" => %w(3d 3-0),
     "稼働2" => %w(2O),
     #"その他" => %w(1他遅 2他遅 3他遅 4他遅 1他急変 2他急変 3他急変 4他急変),
     "他遅" => %w(1他遅 2他遅 3他遅 4他遅), 
     "他急変1" => %w(1他急変1 2他急変1 3他急変1 4他急変1),    "他急変2" => %w(1他急変2 2他急変2 3他急変2 4他急変2),
     "未分類" => nil
    }
-  PaternsKey = %w(稼働無 稼働4一時低下 稼働4 稼働4→3 稼働3 稼働3一時低下 稼働3→2 稼働2 他遅 他急変1 他急変2 未分類)
+  PaternsKey = %w(稼働無 稼働2 稼働3 稼働4 稼働3→2 稼働4→3 稼働3一時低下 稼働4一時低下 他遅 他急変1 他急変2 未分類)
   Shapes = Shimada::Power.all.map(&:shape).compact.uniq
   AllPatern = %w(0 1 2 3 4 5).product(Shapes).map{ |l,s| l+s } 
   Un_sorted = AllPatern - Paterns.values.flatten
@@ -33,7 +39,7 @@ module Shimada::Patern
 
   def shape_calc
     return nil unless lines
-    if lines < 2  ; "S"
+    if lines < 2  ; self.deform = "S"
     #elsif max_diff_from_average_difference > 200 ; "他急変1"
     #elsif [diffdiff(3..20).max,-diffdiff(3..20).min].max >  190 ; "他急変2"
     elsif discriminant.abs < 0.000002       ;"00"
@@ -45,13 +51,13 @@ module Shimada::Patern
         "O"
      # end
     elsif discriminant < 0.0                ; "F"
-    elsif y1     >  Err && y2.abs <   Err   ;  "+0"
-    elsif y1     >  Err && y2     >   Err   ;  "++"
-    elsif y1     >  Err && y2     <  -Err   
-      #max_powers[0] - min_powers[0]  > 120 ? "H" : 
-      "+-"
+    elsif y1     >  Err && y2.abs <   Err   ;  "+0"  
+    elsif y1     >  Err && y2     >   Err   ;  "++" 
     elsif y1     < -Err && y2.abs <   Err   ;  "-0"
-    elsif y1     < -Err && y2     <  -Err   ;  "--"
+    elsif y1     < -Err && y2     <  -Err   
+      if difference_peak_sholder > 100 ;self.deform =  "D" ; "--D"
+      else ; self.deform =  "F" ; "--F"
+      end
     elsif y1     < -Err && y2     >   Err   # -+
       pw_values = pw_peaks
       #unless f3_solve.all?{ |x| Shimada::Power::PolyFitHour.include?(x+Shimada::Power::PolyFitX0)}
@@ -66,7 +72,7 @@ module Shimada::Patern
    #   x0 = f3_solve((x1+x2)*0.5)
     #  max_powers[0] - min_powers[0] > 150 ? "H" :
       "0+"
-    elsif y1.abs <  Err && y2     <  -Err   ;  "0-"
+    elsif y1     >  -Err && y2     <  -Err   ;  "+-" # と 0-
     else      ;   "他"
     end
   end
