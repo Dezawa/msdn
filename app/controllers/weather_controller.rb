f451b3ecfd8177a7847a298ee6b7c22db49f02b5
Dezawa Shinichiro <dezawa@aliadne.net>
2014-07-15 19:18:32 +0900

    気温の他に、湿度、気圧も取り込む

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -7,14 +7,15 @@ class WeatherController < ApplicationController
             HtmlText.new(:location     ,"場所",    :ro => true , :size => 7),
 #            HtmlDate.new(:month        ,"年月",  :tform => "%Y/%m", :ro => true, :size => 7 ),
             HtmlDate.new(:date         ,"年月日",  :tform => "%Y/%m/%d", :ro => true, :size => 7 )
-            ] +
-    Weather::Temperature.map{ |h| 
-      HtmlNum.new(h, h.sub(/hour/,""),:ro => true,:size => 2 ) }
+            ] 
+  Temperature = Weather::Temperature.map{ |h| HtmlNum.new(h, h.sub(/hour/,""),:ro => true,:size => 2 ) }
+  Vaper = Weather::Vaper.map{ |h| HtmlNum.new(h, h.sub(/vaper/,""),:ro => true,:size => 2 ) }
+  Humidity = Weather::Humidity.map{ |h| HtmlNum.new(h, h.sub(/humidity/,""),:ro => true,:size => 2 ) }
 
   def set_instanse_variable
     @Model= Weather
-    @TYTLE = "温度"
-    @labels=Labels
+    @TYTLE = "気象 "
+    #@labels=Labels
     @TableEdit = nil
     @Domain= @Model.name.underscore
     #@TableHeaderDouble = [3,[24,"時刻"]]
@@ -24,17 +25,40 @@ class WeatherController < ApplicationController
   def index
     @labels =  [
             HtmlText.new(:location     ,"場所",    :ro => true , :size => 7),
-            HtmlDate.new(:month        ,"年月",  :tform => "%Y/%m", :ro => true, :size => 7 )
+            HtmlDate.new(:month        ,"年月",  :tform => "%Y/%m", :ro => true, :size => 7 ),
             ]
-    @Show = true
+    @Show = false
+    @Delete = true
     @models = Weather.all(:select => :month,:select => "distinct month,location",
                           :order => "location,month")
   end
 
-  def monthly
+  def temperatuer
     @models = Weather.
       all(:conditions => ["location = ? and month = ?", params[:location],params[:month]],
           :order => "date")
+    @labels = Labels + Temperature
+    @TYTLE = "気温 "
     render  :file => 'application/index',:layout => 'application'
   end
+
+  def vaper
+    @models = Weather.
+      all(:conditions => ["location = ? and month = ?", params[:location],params[:month]],
+          :order => "date")
+    @labels = Labels + Vaper
+    @TYTLE = "蒸気圧 "
+    render  :file => 'application/index',:layout => 'application'
+  end
+
+  def humidity
+    @models = Weather.
+      all(:conditions => ["location = ? and month = ?", params[:location],params[:month]],
+          :order => "date")
+    @labels = Labels + Humidity
+    @TYTLE = "湿度 "
+    render  :file => 'application/index',:layout => 'application'
+  end
+  def delete;end
+
 end
