be256b42c0a3792dc3380d41639213c65008fdc0
Dezawa Shinichiro <dezawa@aliadne.net>
2014-07-17 04:45:26 +0900

    明日予測を途中

--- app/controllers/shimada/factory_controller.rb
+++ app/controllers/shimada/factory_controller.rb
@@ -8,7 +8,7 @@ class Shimada::FactoryController <  Shimada::Controller
      HtmlLink.new(:id,"",:link => { :url => "/shimada/factory/today",:link_label => "本日実績"}),
      HtmlLink.new(:id,"",:link => { :url => "/shimada/factory/tomorrow",:link_label => "明日予報"}),
      HtmlLink.new(:id,"",:link => { :url => "/shimada/month/results",:link_label => "過去実績"}),
-     HtmlLink.new(:id,"",:link => { :url => "/shimada/month/factory",:link_label => "分析"})
+     HtmlLink.new(:id,"",:link => { :url => "/shimada/month/analyze",:link_label => "分析"})
     ]
   def set_instanse_variable
     super
@@ -25,4 +25,26 @@ class Shimada::FactoryController <  Shimada::Controller
     factory = @Model.find params[:id]
     @power = Shimada::Power.find_by_month((Time.now-1.year))
   end
+
+  def tomorrow
+    factory = @Model.find params[:id]
+    @today= Time.now.to_date
+    @tomorrow = @today.tomorrow.to_date
+    #month = tomorrow.beginning_of_month
+    #shimada_month = Shimada::Month.find_or_create_by_month(month)
+    @power = Shimada::Power.find_or_create_by_date(@tomorrow)
+    #@power.month = shimada_month
+    @date = @tomorrow
+    @power.line = 3
+    @power.tomorrow_graph()
+    forecast = Forecast.find_or_fetch(:maebashi,@tomorrow,@today)
+    @temp = forecast.temperature
+    @humi = forecast.humidity
+  end
+
+  def show_gif
+    graph_file = params[:graph_file].blank? ? "tomorrow" : params[:graph_file]
+    send_file RAILS_ROOT+"/tmp/shimada/giffiles/#{graph_file}.gif", :type => 'image/gif', :disposition => 'inline'
+  end
+
 end
