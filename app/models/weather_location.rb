84a4474d90637a5fd28970fae744d8ffa349dbd3
Dezawa Shinichiro <dezawa@aliadne.net>
2014-08-08 07:47:38 +0900

    選択肢を返すclassメソッド作成

--- app/models/weather_location.rb
+++ app/models/weather_location.rb
@@ -1,4 +1,13 @@
 class WeatherLocation < ActiveRecord::Base
   extend Function::CsvIo
+  def self.name_location
+    all(:order =>"forecast_code").map{|wl| [wl.name,wl.location]}
+  end
+
+  def self.name_location_past
+    all(:conditions => "weather_prec is not null",:order =>"forecast_code").
+      map{|wl| [wl.name,wl.location]}
+  end
+
 
 end
