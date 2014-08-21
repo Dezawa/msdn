e22172d391dda4140853e49397fa0eb97e9bb6fc
Dezawa Shinichiro <dezawa@aliadne.net>
2014-08-21 22:00:19 +0900

    グラフファイルの出力先の指定をdirとbasenameに分ける

--- app/models/function/gnuplot.rb
+++ app/models/function/gnuplot.rb
@@ -1,19 +1,39 @@
 # -*- coding: utf-8 -*-
 module Function
 module Gnuplot
+  #
   # option
-  #   必須  :column_labels   => %w(日 中央 - + 気温),          必須
-  #   必須  :column_format     => "%s %.1f %.1f %.1f %.1f\n",
-  #         :axis_labels => { :xlabel => "日",:ylabel => "推定電力",:y2label => "気温"},
-  #         :title  => "電力推定",
-  #         :tics   => { :xtics => "rotate by -90",:y2tics=> "-5,5"},
-  #         :by_tics => { 4 => "x1y2" },
-  #         :size   => "900,400",             
-  #         :grid   => "ytics"                
+  #   必須  :column_labels   例  %w(日 中央 - + 気温),          必須
+  #   必須  :column_format   例 "%s %.1f %.1f %.1f %.1f\n",
+  #         :terminal        default    "jpeg"
+  #         :
+  #         :axis_labels     例  { :xlabel => "日",:ylabel => "推定電力",:y2label => "気温"},
+  #         :title  例 => "電力推定",
+  #         :tics   例 => { :xtics => "rotate by -90",:y2tics=> "-5,5"},
+  #         :by_tics 例  => { 4 => "x1y2" },    x1y1以外の軸を使うとき
+  #         :size             default  "600,400"
+  #         :grid   例 => "ytics"                
+  #         :additional_lines  近似線などを書く式を生で記述
+  #  実装まだ
   #         :type   => "scatter"  using 1:2,  無いときは using 2:xticlabel(1)
   #         :with   => "line",
+  #
+  #         :group_by    data_list.group_by{ |d| d.semd(opt[:group_by])}
+  #         :keys        defaultではgroup_by の分類がsortされて使われる。
+  #                      違うsort順にしたいときに設定
+  #  ファイルpath関連
+  #         :base_path              データ書き出しパス。RAILS_ROOTからの相対 tmp/gnuplot/data
+  #         :graph_file                "image"
+  #         :graph_file_dir             RAILS_ROOT+"/tmp"
+  #         :define_file           絶対path  RAILS_ROOT+"/tmp/gnuplot/graph.def"
+  # 出力される画像fileは  '#{opt[:graph_file_dir]}/#{opt[:graph_file]}.#{opt[:terminal]}'
   def gnuplot_(data_list,opt)
-    opt = { }.merge opt
+    opt = { :terminal => "jpeg",:size => "600,400" ,
+      :graph_file => "image", :graph_file_dir => RAILS_ROOT+"/tmp",
+      :define_file => RAILS_ROOT+"/tmp/gnuplot/graph.def",
+      :base_path   =>  "tmp/gnuplot/data"
+    }.merge opt
+
     group_by = opt[:group_by] ? data_list.group_by{ |d| d.semd(opt[:group_by])} : [["",data_list]]
     path = output_datafile(group_by,opt){ |f,k,data|
       data.each{ |datum| f.printf opt[:column_format],*datum}
@@ -23,9 +43,6 @@ module Gnuplot
   end
 
   def output_gnuplot_define(path,opt)
-    opt = { :terminal => "jpeg",:size => "600,400" ,:graph_file => "image",
-      :graph_file_dir => RAILS_ROOT+"/tmp"
-    }.merge opt
     
     head = "set terminal #{opt[:terminal]} enhanced size #{opt[:size]} enhanced font 'usr/share/fonts/truetype/takao/TakaoPGothic.ttf,10'
 set out '#{opt[:graph_file_dir]}/#{opt[:graph_file]}.#{opt[:terminal]}'
@@ -39,7 +56,7 @@ set title '#{opt[:title]}"
                       when "scatter"  ; plot_scatter path,opt
                       else            ; plot_by_column path,opt
                       end
-    def_file = opt[:define_file] || RAILS_ROOT+"/tmp/gnuplot/graph.def"
+    def_file = opt[:define_file]
     open(def_file,"w"){ |f|
       f.puts head,key
       [range,tics,grid,axis_labels].each{ |item| f.puts item if item }
@@ -76,7 +93,7 @@ set title '#{opt[:title]}"
   end
 
   def output_datafile(grouped_data_ary,opt,&block)
-    base_path = (opt[:base_path] || "tmp/gnuplot/data")+"000"
+    base_path = opt[:base_path]+"000"
     path = []
     keys = opt[:keys] || grouped_data_ary.map{ |ary| ary.first}.sort
     keys.each_with_index{ |k,idx|
