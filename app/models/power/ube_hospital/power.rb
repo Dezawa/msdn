1ecb70528151558db7d0fb5b3ca304cccd51c711
Dezawa Shinichiro <dezawa@aliadne.net>
2014-08-23 13:36:03 +0900

    天候地域登録

--- app/models/power/ube_hospital/power.rb
+++ app/models/power/ube_hospital/power.rb
@@ -5,4 +5,7 @@ class Power::UbeHospital::Power < ActiveRecord::Base
   belongs_to :month     ,:class_name => "Power::UbeHospital::Month"
   belongs_to :db_weather,:class_name => "Weather" 
 
+  def self.weather_location; "ube" ;end
+  def self.weather_location2; "shimonoseki" ;end
+
 end
