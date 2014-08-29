b15384e0f15ea479ed7634aba79968de4f4cff98
Dezawa Shinichiro <dezawa@aliadne.net>
2014-08-29 22:00:24 +0900

    電力の時間を追ったグラフ

--- app/models/power/ube_hospital/month.rb
+++ app/models/power/ube_hospital/month.rb
@@ -8,17 +8,17 @@ class Power::UbeHospital::Month < ActiveRecord::Base
   extend Power::Scatter
   set_table_name 'power_ube_hospital_months'
   has_many :powers ,:class_name =>  "Power::UbeHospital::Power" ,
-           :dependent => :delete_all
-@@ave10hour = { }
+  :dependent => :delete_all
+  @@ave10hour = { }
   
   class << self
     def power_model ;   Power::UbeHospital::Power ;end
 
     def ave10hour(year_month)
       @@ave10hour[year_month] ||= if true
-        Power::UbeHospital::Month.find_by_month(year_month).
-          powers.map(&:rev10).average
-      end
+                                    Power::UbeHospital::Month.find_by_month(year_month).
+                                      powers.map(&:rev10).average
+                                  end
     end
 
     def search_year_month(lines)
@@ -36,16 +36,110 @@ class Power::UbeHospital::Month < ActiveRecord::Base
         raise RuntimeError,"時刻が合わない: #{line}" if idx+1 != clms.shift.to_i
         powers.each{ |power| power[hour] = clms.shift.to_f }
       }
-         line=lines.shift until /袋数/ =~ line
-logger.debug("SET_POWER:#{line}")
+      line=lines.shift until /袋数/ =~ line
       clms = line.split(",")
       clms.shift
       powers.each{ |power| power[:hukurosu] = clms.shift.to_f ;power.save}
     end
 
+    Keys =                     %w(平日昼  休日昼   平日夜     休日夜     10時気温     日)
+    KeyValues = Hash[ *Keys.zip([[2,:day],[1,:day],[2,:night],[1,:night],[nil,:temp],[nil,nil]]).flatten(1)]
+    # 与えられたデータの24時間のデータを全て時間順に並べる
+    # 9-16時と1-5時のものだけプロットする。
+    # 横軸は時間単位。ただし軸は月初日付で表示する。
+    # group は、平日昼 平日夜 休日昼 休日夜
+    def graph_by_days_hour(objects,opt={ })
+      # keys[平日昼] => [line=1,time_zone = :day]
+      days = objects.map{ |pw| pw.date}
+      min,max = [days.min,days.max]
+      x2range = "[#{min.yday}:#{max.yday}]"
+      opt = { :xlabel => "xl '日'",:ylabel => "yl '補正後 消費電力'", :y2label => "y2label '外気温'",
+        :keys => Keys,
+        :tics => "set xtics nomirror rotate by -90 scale 0
+set x2range #{x2range}
+set ytics 0,100,800",
+        :by_tics => %w(x2y1 x2y1 x2y1 x2y1)
+      }.merge opt
+      opt[:method] ||= :revise_by_temp
+      path = opt[:keys].map{ |key| output_plot_day_hour_data(objects,opt,key) }
+
+      def_file = def_file_by_days_hour(path,opt.merge(:min => min,:max => max))
+      `(cd #{RAILS_ROOT};/usr/local/bin/gnuplot #{def_file})`
+    end
+
+    Hours = { :night => (0 .. 4) , :day => (8..15)}
+    def  output_plot_day_hour_data(objects,opt,key)
+      line,timezone = KeyValues[key]
+      
+logger.debug("OUTPUT_PLOT_DAY_HOUR_DATA : opt = #{opt.to_a.flatten.join(', ')}")
+      path = RAILS_ROOT+"/tmp/graph/data/graphdata_#{line}#{timezone}"
+      objes = objects.select{ |pw| pw.line == line}
+      open(path,"w"){ |f|
+        f.puts "日 #{key}"
+        if line
+          objes.each{ |pw|
+            Hours[timezone].each{ |h|
+              f.printf "%.2f %f\n",pw.date.yday+ h/24.0 , pw.send(opt["method"])[h]
+            }
+          }
+        elsif timezone == :temp
+          days = objects.map{ |pw| pw.date}
+          objects.each{ |pw| 
+            [9].each{ |h| 
+              f.puts "#{pw.date.yday+h/24.0} #{pw.temps[h]}"
+            }
+          }
+        else
+          days = objects.map{ |pw| pw.date}
+          min,max = [days.min,days.max]
+          (min..max).each{ |day| 
+            f.puts( [1,11,21].include?(day.day) ? "#{day.strftime('%m/%d')} 0" : ". 0")
+            (2..24).each{  f.puts ". 0"}
+          }
+        end
+      }
+      path
+    end
+
+    def  def_file_by_days_hour(path,opt={ })
+      opt.merge!( :point_size => 0.7,:point_type => [7,7,7,7,5,5,6,6,6,6])
+      deffile = ( opt[:def_dir] || RAILS_ROOT+"/tmp/graph")+"/"+(opt[:def_file] || "graph.def" )
+      graph_dir,graph_file,title,set_key,xrange,tics = dif_opts(opt)
+      open(deffile,"w"){ |f|
+        preunble = DefByDayHour%[graph_dir,graph_file,title,set_key,"[100:800]",xrange,tics ]
+        f.puts preunble
+        f.puts( "set x2tics ("+
+               (opt[:min] .. opt[:max]).map{ |day|
+                 next unless [1,11,21].include?(day.day)
+                  day.yday
+               }.compact.join(" , ") + ")"
+                )
+        [:xlabel,:ylabel,:x2label,:y2label].each{ |sym|
+          f.puts "set "+opt[sym] if opt[sym]
+        }
+        f.print plot_list("'%s' using %d:%d",path[0,4],opt)
+        f.puts " ,\\\n '#{path[-2]}' using 1:2 with lines axis x2y2 ,\\"
+        f.puts " '#{path.last}' using 2:xtic(1) notitle "
+      }
+      deffile
+    end
+
   end # of class method
 
   def ave10hour
-      self.class.ave10hour(month)
+    self.class.ave10hour(month)
   end
+
+  DefByDayHour =
+    %Q!set terminal jpeg enhanced size 800,400 enhanced font "/usr/share/fonts/truetype/takao/TakaoPGothic.ttf,10"
+set out '%s/%s.jpeg' #  graph_dir,graph_file,
+set title "%s"
+%s #  ,set_key
+set yrange %s #[0:1000]
+#set xrange %s #  [1:24]
+set y2range [5:40]
+%s # tics
+set y2tics
+set grid x2tics ytics
+!
 end
