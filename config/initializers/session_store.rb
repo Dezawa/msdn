a513d4702e56cfc9d6925508a95f54db387cd984
Dezawa Shinichiro <dezawa@aliadne.net>
2014-09-08 22:26:41 +0900

    RAILS41_立ち上げ途中

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-#Msdn::Application.config.session_store :cookie_store, key: '_msdn_session'
-Msdn::Application.config.session_store :active_record_store, key: '_msdn_session'
+Rails.application.config.session_store :cookie_store, key: '_msdnrails41_session'
