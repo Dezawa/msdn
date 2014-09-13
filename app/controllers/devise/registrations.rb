00152f43953c834131b73e48b7ffa19199f99903
Dezawa Shinichiro <dezawa@aliadne.net>
2014-09-13 19:27:49 +0900

    一応、login logout できた

--- app/controllers/devise/registrations.rb
+++ app/controllers/devise/registrations.rb
@@ -1,18 +1,3 @@
 class Devise::RegistrationsController  < ApplicationController
 
-  def create
-    @user = User.new(permit_attr)
-    success = @user && @user.save
-    logger.debug("UserCreate success? #{success}")
-    #logger.debug("UsetCreate #{@user.errors.on(:email)}")
-    if success && @user.errors.empty?
-        redirect_to :action => :index
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
-    else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-    @arrowed_options = @user.user_options.map(&:id) || []
-      render :action => 'new'
-    end
-  end #of create
-
 end
