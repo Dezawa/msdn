355c5a4381e04052628eb415f2375c5d921cce4f
Dezawa Shinichiro <dezawa@aliadne.net>
2014-09-14 12:36:57 +0900

    イメージ表示のmethodをCommonControllerに

--- app/controllers/common_controller.rb
+++ app/controllers/common_controller.rb
@@ -1,3 +1,12 @@
 class CommonController < ApplicationController
  include Actions
+
+  
+   def show_img
+     graph_dir  = Rails.root+
+       (params[:graph_dir].blank? ? "tmp/img"  :  params[:graph_dir])
+     graph_file = graph_dir + (params[:graph_file].blank? ? "power" : params[:graph_file])
+     graph_format = params[:graph_format].blank? ? :gif : params[:graph_format]
+     send_file "#{graph_file}.#{graph_format}", :type => "#{graph_format}", :disposition => 'inline'
+   end
 end
