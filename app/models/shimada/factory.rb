97dc7f9c75da3b79cda77d0113220963ca185db3
Dezawa Shinichiro <dezawa@aliadne.net>
2014-09-27 18:07:11 +0900

    中部は電力が30分おきにあったのでそれを使うつもりで DB table shimada_power_by_30mins をつくり、それを Shimada::Cubu::Poser にしていた。 しかしそれだとグラフ化の修正に手間がかかりそうなので、 最初の版は1時間毎で行うことにした。 そのため、Shimada::Chubu::Monthの has_many はShimada::Cubu::Poser だが そのtableはshimada_powersとした。 それにともない、testも修正した

--- app/models/shimada/factory.rb
+++ app/models/shimada/factory.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-Shimada::PowerModels = [Shimada::Power,Shimada::PowerBy_30min] # power_model_idで探す
-#Shimada::PowerModels = [Shimada::Power,Shimada::Power] # power_model_idで探す
+#Shimada::PowerModels = [Shimada::Power,Shimada::PowerBy_30min] # power_model_idで探す
+Shimada::PowerModels = [Shimada::Power,Shimada::Power] # power_model_idで探す
 Shimada::MonthModels = [Shimada::Month,Shimada::Chubu::Month]  # power_model_idで探す
 
 class Shimada::Factory < ActiveRecord::Base
