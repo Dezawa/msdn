cc05e0693c59d5ae30cdf3f7f6f384f90c655f56
Dezawa Shinichiro <dezawa@aliadne.net>
2014-09-28 16:02:24 +0900

    関連定義

--- app/models/shimada/factory.rb
+++ app/models/shimada/factory.rb
@@ -4,6 +4,6 @@ Shimada::PowerModels = [Shimada::Power,Shimada::Power] # power_model_idで探す
 Shimada::MonthModels = [Shimada::Month,Shimada::Chubu::Month]  # power_model_idで探す
 
 class Shimada::Factory < ActiveRecord::Base
-  
+  has_many :shimada_powers ,:class_name =>  "Shimada::Power"
 end
 
