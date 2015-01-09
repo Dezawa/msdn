f7ca56d1b93b853795fda4791662ec5b2a8092e9
Dezawa Shinichiro <dezawa@aliadne.net>
2015-01-09 18:32:57 +0900

    table ondotori_status が libのOndotoriと衝突するので名前を変えたいのだが、うまくいかないのでDBを作り直した.

---
+++ app/controllers/status/ondotori_controller.rb
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+class Status::OndotoriController < CommonController#ApplicationController
+  include Actions
+  before_filter :set_instanse_variable
+  Labels =
+    [HtmlText.new(:base_name                ,"Base", :ro => true),
+       HtmlText.new(:group_name               ,"Group", :ro => true),
+       HtmlText.new( :group_remote_name       ,"Remote", :ro => true),
+       HtmlText.new(:group_remote_ch_name     ,"Ch"  , :ro => true),
+       HtmlText.new(:group_remote_ch_record_type,"型", :ro => true),
+       HtmlDate.new(:group_remote_ch_unix_time,"日時" ,:ro => true,:tform =>"%Y/%m/%d %H:%M" ),
+       HtmlText.new( :group_remote_rssi       ,"電波" , :ro => true), 
+       HtmlText.new(:group_remote_ch_current_batt ,"電池残量", :ro => true),
+      ]
+
+  def set_instanse_variable
+    super
+    @Model= Status::Ondotori
+    @Domain= @Model.name.underscore
+    @TYTLE = "おんどとり Currentデータ"
+    @Delete=true
+    @FindOrder = "group_remote_ch_unix_time"
+  end
+
+  def list
+    @models =
+      @Model.select_each_one_from_every_group_by([:base_name, :group_name, :group_remote_name],
+                                                 "group_remote_ch_unix_time DESC")
+    @labels = Labels
+    @Delete = false
+    render  :file => 'application/index',:layout => 'application'
+  end
+
+  def index
+    @labels = Labels
+    super
+  end
+
+  def load_local_file
+    @Model.load_xml params[:uploadfile]
+    redirect_to :action => :index
+  end
+end
