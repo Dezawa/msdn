cd96cbc3a6d9076c5fdee94ee1e173d08e682e61
Dezawa Shinichiro <dezawa@aliadne.net>
2015-01-24 22:03:20 +0900

    Hospitalにて病院の定義データの項目についての定義を、共通controllerからHospital::Constに移した

--- app/controllers/hospital/bushos_controller.rb
+++ app/controllers/hospital/bushos_controller.rb
@@ -16,54 +16,12 @@ class Hospital::BushosController < Hospital::Controller
                       HtmlText.new(:nil,"",:ro=>true),
                       HtmlText.new(:nil,"項目",:ro=>true),HtmlText.new(:nil,"値",:ro=>true)
                    ]
-    @ItemsDefine =
-      [HtmlText.new(:hospital_name ,"保険医療機関名"),
-       HtmlSelect.new(:hospital_Koutai ,"交代勤務",  :correction => %w(二交代 三交代)     ),
-       HtmlText.new(:hospital_bed_num ,"病床数"    ,:size =>3  ),
-       HtmlText.new(:kubun          ,"届出区分"      ,:size =>3, :comment => "対１入院基本料"),
-       HtmlSelect.new(:KangoHaichi_addition,"看護配置加算の有無" ,  :correction => %w(有 無),:include_blank=> true),
-       HtmlSelect.new(:Kyuuseiki_addition  ,"急性期看護補助体制加算の届出区分",:correction => %w(25 50 75),:include_blank=> true),
-       HtmlSelect.new(:Yakan_Kyuuseiki_addition,"夜間急性期看護補助体制加算の届出区分",:correction => %w(50 100),:include_blank=> true),
-       HtmlSelect.new(:night_addition  ,"看護職員夜間配置加算の有無"  ,  :correction => %w(有 無),:include_blank=> true),
-       HtmlSelect.new(:KangoHojo_additon   ,"看護補助加算の届出区分",:correction => %w(30 50 75),:include_blank=> true),
-       HtmlText.new(:weekly_hour,"常勤職員の週所定労働時間",:size => 3)
-      ]
-    @ItemsDefine2 =
-      [[HtmlText.new(:patient_num        ,"届出時入院患者数"  ,:size =>3,:align => :right)],
-       [HtmlText.new(:average_patient    ,"１日平均入院患者数",:size =>3,:align => :right)],
-       [HtmlText.new(:patient_start_year ,"算出期間 年"       ,:size =>3,:align => :right),
-        HtmlText.new(:patient_start_month,"月～"              ,:size =>1,:align => :right),
-        HtmlText.new(:patient_stop_year  ,"年"                ,:size =>3,:align => :right),
-        HtmlText.new(:patient_stop_month ,"月"                ,:size =>1,:align => :right)],
-       [HtmlText.new(:average_Nyuuin     ,"平均在院日数"      ,:size =>3,:align => :right)],
-       [HtmlText.new(:Nyuuin_start_year  ,"算出期間 年"       ,:size =>3,:align => :right),
-        HtmlText.new(:Nyuuin_start_month ,"月～"              ,:size =>1,:align => :right),
-        HtmlText.new(:Nyuuin_stop_year   ,"年"                ,:size =>3,:align => :right),
-        HtmlText.new(:Nyuuin_stop_month  ,"月"                ,:size =>1,:align => :right)]
-    ]
-    @ItemsAll =  (@ItemsDefine + @ItemsDefine2).flatten
+ 
 end
 
   def index
-#<<<<<<< HEAD:app/controllers/hospital/bushos_controller.rb
-    @instances = Hospital::Define.all
-    regesterd = @instances.map(&:attri)
-    need      = @ItemsDefine.map{ |l| l.symbol.to_s }
-# =======
-#     instances = Hospital::Define.all
-#     regesterd = instances.map(&:attri)
-#     need      =  @ItemsAll .map{ |l| l.symbol.to_s }
-#>>>>>>> HospitalPower:app/controllers/hospital/busho_controller.rb
-    lack = (need - regesterd)
-    creeat = @ItemsAll.map{ |label|
-      if lack.include?(label.symbol.to_s)
-         Hospital::Define.create( :name => label.label,
-                                  :attri => label.symbol.to_s,
-                                  :comment   => label.comment
-                                  )
-      end
-    }.compact
-    @instances = Hash[*(instances+creeat).map{ |model| [model.attri.to_sym,model]}.flatten]
+    @instances = Hospital::Define.find_or_create_all.
+      map{ |model| [model.attri.to_sym,model]}.to_h
     super
   end
 
