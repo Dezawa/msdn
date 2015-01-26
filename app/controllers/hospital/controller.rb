c4c00cfb8a3c04812aa62ada538653f3d1d55f88
Dezawa Shinichiro <dezawa@aliadne.net>
2015-01-26 18:39:31 +0900

    Hospital::Controllerでのsessionの内容をmodelそのままではなく、YAMLdumpするようにした。modelそのままだと、model.to_sの内容が文字列で入るだけで、復元してくれない

--- app/controllers/hospital/controller.rb
+++ app/controllers/hospital/controller.rb
@@ -35,7 +35,7 @@ class Hospital::Controller < CommonController #ApplicationController
               Menu.new("役割一覧",:roles),
               Menu.new("禁忌",:avoid_combination),
               Menu.new("部署登録",:bushos), 
-              Menu.new("必要人数",:need),
+              Menu.new("必要人数",:needs),
               Menu.new("個人登録",:nurces), 
               Menu.new("役割割当",:roles, :action => :show_assign),
               Menu.new("会議登録",:meetings),
@@ -44,8 +44,8 @@ class Hospital::Controller < CommonController #ApplicationController
               Menu.new("様式9",:form9,:action => :calc),
               Menu.new("休日",:holyday,:controller => "/holyday",:action => :index,:page=>1,:id => Time.now.year)
              ]
-    #@busho_getudo = Marshal.load(session[:hospital] ||= Marshal.dump(BushoGetudo.new))
-    @busho_getudo = Marshal.load( Marshal.dump(BushoGetudo.new))
+    @busho_getudo = YAML.load(session[:hospital] ||= YAML.dump(BushoGetudo.new))
+    # @busho_getudo = YAML.load( YAML.dump(BushoGetudo.new))
     @month = @busho_getudo.month 
     if @busho_getudo.busho_id 
       @current_busho_id = @busho_getudo.busho_id #session[:hospital_busho] || 1
@@ -57,8 +57,12 @@ class Hospital::Controller < CommonController #ApplicationController
 
   def set_busho_sub
     @busho_getudo.busho_id = params[:busho_getudo][:busho_id].to_i 
-    session[:hospital] = Marshal.dump @busho_getudo
+    session[:hospital] = YAML.dump @busho_getudo
     #logger.debug "session[:hospital]: #{session[:hospital].busho_id}"
+  end
+  def set_busho_sub
+    @busho_getudo.busho_id = params[:busho_getudo][:busho_id].to_i 
+    session[:hospital] = YAML.dump @busho_getudo
   end
   def set_busho
     set_busho_sub
