f8ae2ae6d3113f2584d3585cdf7e1adf9219b420
Dezawa Shinichiro <dezawa@aliadne.net>
2015-01-28 06:34:02 +0900

    Hospital::Roleのmodelテスト

--- test/models/hospital/role_test.rb
+++ test/models/hospital/role_test.rb
@@ -3,7 +3,19 @@ require 'test_helper'
 # -*- coding: utf-8 -*-
 
 class Hospital::RoleTest < ActiveSupport::TestCase
-#  fixtures :hospital_roles
+  fixtures "hospital/roles"
 
+  must "Hospital::Role.shokushu" do
+    assert_equal [["看護師", 2], ["準看護師", 3]],Hospital::Role.shokushu
+  end
+  must "Hospital::Role.kinmukubun" do
+    assert_equal [["日勤", 7], ["三交代", 8], ["二 交代", 9], ["パート", 10]],Hospital::Role.kinmukubun
+  end
+  must "Hospital::Role.shikaku" do
+    assert_equal [["Aチーム", 4], ["Bチーム", 5]],Hospital::Role.shikaku
+  end
+  must "Hospital::Role.shokui" do
+    assert_equal [["リーダー", 1], ["看護師長", 6]],Hospital::Role.shokui
+  end
 end
 # -*- coding: utf-8 -*-
