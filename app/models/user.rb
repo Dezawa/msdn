5bb0fa67e92d38bcc92e873ce78ed0f3fa4f18f3
Dezawa Shinichiro <dezawa@aliadne.net>
2015-01-30 15:35:57 +0900

    Usersのdefaulsの@labelsを復活   passwoedの要否があるので、action毎に変えるようにしたが、   defauldで良いもののactionが多く、付け忘れがめだったので

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :encryptable, :encryptor => :restful_authentication_sha1
 
