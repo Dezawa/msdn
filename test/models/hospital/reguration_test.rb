1425e3be248f0165f338550f69cde0bbf8483ecc
Dezawa Shinichiro <dezawa@aliadne.net>
2015-02-12 13:36:23 +0900

    Regration.newの引数をHashに変更。

--- test/models/hospital/reguration_test.rb
+++ test/models/hospital/reguration_test.rb
@@ -15,7 +15,7 @@ class Hospital::RegurationTest < ActiveSupport::TestCase
   shift1 = "0000_0001111110000000"
   (4..9).each{ |day|
   must "renkin #{day}" do
-    reguration = Hospital::Regrate.create(/[1-8LM]{6,}/,5,11,"連続勤務5日まで")
+    reguration = Hospital::Regrate.create(/[1-8LM]{6,}/,back: 5,length: 11,comment: "連続勤務5日まで")
     assert reguration.check(day,shift1)
 
     reguration = @check_reg[0][:renkin]
@@ -24,7 +24,7 @@ class Hospital::RegurationTest < ActiveSupport::TestCase
   }
   [1,2,3,10,11,12].each{ |day|
   must "renkin #{day}" do
-    reguration = Hospital::Regrate.create(/[1-8LM]{6,}/,5,11,"連続勤務5日まで")
+    reguration = Hospital::Regrate.create(/[1-8LM]{6,}/,back: 5,length: 11,comment: "連続勤務5日まで")
     assert !reguration.check(day,shift1)
 
     reguration = @check_reg[0][:renkin]
@@ -36,7 +36,7 @@ class Hospital::RegurationTest < ActiveSupport::TestCase
   shift2 = "0000_00011101110000000" 
   (1..12).each{ |day|
   must "renkin #{day}" do
-    reguration =  Hospital::Regrate.create(/[1-8LM]{6,}/,5,11,"連続勤務5日まで")
+    reguration =  Hospital::Regrate.create(/[1-8LM]{6,}/,back: 5,length: 11,comment: "連続勤務5日まで")
     assert !reguration.check(day,shift2)
 
     reguration = @check_reg[0][:renkin]
@@ -47,27 +47,30 @@ class Hospital::RegurationTest < ActiveSupport::TestCase
   shift3 = "0000_0002210231_330_1101110000000"
   [4,5,6,8,9,10].each{ |day|
   must "after_nights #{day}" do
-    reguration =  Hospital::Regrate.create(/[2L3M56]{2}[^0_]/,2,5,"連続夜勤明けは休み")
+    reguration =  Hospital::Regrate.create(/[2L3M56]{2}[^0_]/,back: 2,length: 5,comment: "連続夜勤明けは休み")
     assert reguration.check(day,shift3)
   end
   }
   [1,2,3,7,11,12,13,14,15,18,19].each{ |day|
   must "after_nights #{day}" do
-    reguration =  Hospital::Regrate.create(/[2L3M56]{2}[^0_]/,2,5,"連続夜勤明けは休み")
+    reguration =  Hospital::Regrate.create(/[2L3M56]{2}[^0_]/,back: 2,length: 5,comment: "連続夜勤明けは休み")
     assert !reguration.check(day,shift3)
   end
   }
 $day=3
   must "kinmu_total 10" do
-    reguration =  Hospital::Regrate.create(/[^1-8LM]/,nil,10, "勤務は22日まで",:through)
+    reguration =  Hospital::Regrate.create(/[^1-8LM]/,length: 10, comment: "勤務は22日まで",
+                                           method: Hospital::Total)
     assert !reguration.check($day,"0000_01234567880100000")
   end
   must "kinmu_total 11" do
-    reguration =  Hospital::Regrate.create(/[^1-8LM]/,nil,10, "勤務は22日まで",:through)
+    reguration =  Hospital::Regrate.create(/[^1-8LM]/,length: 10, comment: "勤務は22日まで",
+                                           method: Hospital::Total)
     assert reguration.check($day,"0000_012345678801020000")
   end
   must "kinmu_total 12" do
-    reguration =  Hospital::Regrate.create(/[^1-8LM]/,nil,10, "勤務は22日まで",:through)
+    reguration =  Hospital::Regrate.create(/[^1-8LM]/,length: 10, comment: "勤務は22日まで",
+                                           method: Hospital::Total)
     assert reguration.check($day,"0000_0123456788010230000")
   end
 end
