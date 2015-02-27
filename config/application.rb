eb9ed4aee02507b3eb00150573096ffd3dedf91f
Dezawa Shinichiro <dezawa@aliadne.net>
2015-02-27 19:16:06 +0900

    hate

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Msdn
     config.active_record.default_timezone = :local
   end
 end
+
 class Array
   def mult(other) ;  self.zip(other).map{ |a,b| a*b } ;end
   def add(other) ;  self.zip(other).map{ |a,b| a+b } ;end
