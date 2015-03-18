8b2387c24dcbda9da85e01af9bd8991107e8f8d3
Dezawa Shinichiro <dezawa@aliadne.net>
2015-03-18 18:58:58 +0900

    HtmlSafeなhtml table のtag定義別建てにした

---
+++ app/helpers/html_safe_table_items.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+module HtmlSafeTableItems
+  TABLE =  "<table>".html_safe
+  TR = "<tr>".html_safe
+  TD = "<td>".html_safe
+  TDTD="</td><td>".html_safe
+  TABLEend =  "</table>".html_safe
+  TRend    = "</tr>".html_safe
+  TDend    = "</td>".html_safe
+end
