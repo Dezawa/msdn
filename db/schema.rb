d5f0a4943fa2cade642133cc4b42529d1ee786b6
Dezawa Shinichiro <dezawa@aliadne.net>
2015-04-21 19:56:56 +0900

    Status::TansDに column serial追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420023600) do
+ActiveRecord::Schema.define(version: 20150421060849) do
 
   create_table "forecasts", force: :cascade do |t|
     t.string   "location",     limit: 255
@@ -547,6 +547,7 @@ ActiveRecord::Schema.define(version: 20150420023600) do
     t.datetime "group_remote_ch_unix_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "serial",                       limit: 255
   end
 
   create_table "todos", force: :cascade do |t|
