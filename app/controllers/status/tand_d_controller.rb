81e836be71a977064afa7c79512feba3cc9fa386
Dezawa Shinichiro <dezawa@aliadne.net>
2015-04-24 20:27:03 +0900

    Tand_D_controller   一覧に      serial 追加      type をselectに変更

--- app/controllers/status/tand_d_controller.rb
+++ app/controllers/status/tand_d_controller.rb
@@ -3,11 +3,13 @@ class Status::TandDController < CommonController#ApplicationController
   include Actions
   before_filter :set_instanse_variable
   Labels =
-    [HtmlText.new(:base_name                ,"Base", :ro => true),
+    [HtmlText.new(:serial                ,"serial", :ro => true),
+     HtmlText.new(:base_name                ,"Base", :ro => true),
        HtmlText.new(:group_name               ,"Group", :ro => true),
        HtmlText.new( :group_remote_name       ,"Remote", :ro => true),
        HtmlText.new(:group_remote_ch_name     ,"Ch"  , :ro => true),
-       HtmlText.new(:group_remote_ch_record_type,"型", :ro => true),
+       HtmlSelect.new(:group_remote_ch_record_type,"型", :ro => true,
+                      :correction =>  Ondotori::TypeName             ),
        HtmlDate.new(:group_remote_ch_unix_time,"日時" ,:ro => true,:tform =>"%Y/%m/%d %H:%M" ),
        HtmlText.new( :group_remote_rssi       ,"電波" , :ro => true), 
        HtmlText.new(:group_remote_ch_current_batt ,"電池残量", :ro => true),
