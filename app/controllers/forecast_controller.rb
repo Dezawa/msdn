219a8a0b8c56a445cd3c6c447306270d9fdf9bbd
Dezawa Shinichiro <dezawa@aliadne.net>
2015-05-22 23:15:46 +0900

    天気予報の一覧をベタから月一覧、日のデータ、一日の全データの階層化を行った

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -5,23 +5,30 @@ class ForecastController < CommonController # ApplicationController
   Weather  = %w(weather03 weather06 weather09 weather12 weather15 weather18 weather21 weather24)
   Humi     = %w(humi03 humi06 humi09 humi12 humi15 humi18 humi21 humi24)
   Vaper    = %w(vaper03 vaper06 vaper09 vaper12 vaper15 vaper18 vaper21 vaper24)
-
-  Labels = [
-            HtmlText.new(:location     ,"場所",    :ro => true , :size => 7),
-#            HtmlDate.new(:month        ,"年月",  :tform => "%Y/%m", :ro => true, :size => 7 ),
-            HtmlDate.new(:date         ,"年月日",  :tform => "%Y/%m/%d", :ro => true, :size => 7 ),
-            HtmlDate.new(:announce ,"発表日",  :tform => "%m/%d %H", :ro => true, :size => 7 )
+  LabelsMonthList =
+    [ HtmlText.new(:location  ,"場所",    :ro => true , :size => 7),
+     HtmlLink.new(:month     ,"年月",  :tform => "%Y/%m", :ro => true, :size => 7 ,
+                  :link => {  :url => "/forecast",
+                            :key => :month, :key_val => :month})
+    ]
+  LabelsDaylies = [
+            HtmlText.new(:location ,"場所",    :ro => true , :size => 7),
+            HtmlLink.new(:date     ,"年月日",  :tform => "%Y/%m/%d", :ro => true, :size => 7,
+                 :link => {  :url => "/forecast",:key => :date, :key_val => :date}
+                        ),
+            HtmlDate.new(:announce ,"発表日時",  :tform => "%m/%d %H", :ro => true, :size => 7 )
             ] +
     Temp.map{  |clm|   HtmlNum.new(clm.to_sym,clm.sub(/temp/,""),:tform => "%.1f") } +
     Vaper.map{ |clm|   HtmlNum.new(clm.to_sym,clm.sub(/vaper/,""),:tform => "%.1f") } +
     Humi.map{  |clm|   HtmlNum.new(clm.to_sym,clm.sub(/humi/,""),:tform => "%.1f") } +
     Weather.map{ |clm| HtmlText.new(clm.to_sym,clm.sub(/weather/,"")) } 
-  
+  LabelsDayAll = LabelsDaylies.dup
+  LabelsDayAll[1,1] = HtmlDate.new(:date ,"年月日",:tform => "%Y/%m/%d",:ro => true,:size => 7)
+    
   def set_instanse_variable
     super
     @Model= Forecast
     @TYTLE = "予報 "
-    @labels=Labels
     correction = WeatherLocation.all.map{|wl| [wl.name,wl.location]}
     #Forecast::ZP.map{ |location,value| [value[1],location]}
     @TableEdit = #[[:input_and_action,"get_data","新規取得 年月(日) 2014-7(-10)",{:size=>8}]]
@@ -37,6 +44,23 @@ class ForecastController < CommonController # ApplicationController
     @FindOrder = "date"
   end
 
+  # 予報月の一覧。
+  def index
+    if params[:month]
+      @labels = LabelsDaylies
+      daylies_of_month
+      render  :file => 'application/index',:layout => 'application'
+    elsif params[:date]
+      @labels = LabelsDayAll
+      daylies_of_a_day
+      render  :file => 'application/index',:layout => 'application'
+    else
+      @TableHeaderDouble = nil
+      @labels = LabelsMonthList
+      month_list 
+    end
+  end
+  
   def show
     if /[^\d]/ =~ params[:id]
       send(params[:id])
@@ -75,4 +99,45 @@ class ForecastController < CommonController # ApplicationController
     render :text => locations.join(",")
   end
 
+  def month_list
+    @Pagenation = 3
+    @page = params[:page] || 1
+    @dayly = @Model.order("date desc").group(:month).
+      paginate( :page =>  @page,:per_page => @Pagenation)
+    @models = @Model.where(:month  => @dayly.map(&:month)).
+      where(:location =>  @weather_location).
+      group_by{ |dayly| dayly.month }.
+      map{|month,daylies| daylies[0]}.reverse
+  end
+
+  def daylies_of_month
+    @models = @Model.where(:month  => params[:month],:location =>  @weather_location).
+      order("date").
+      group_by{ |d| d.date }.
+      map{|date,daylies| daylies.sort_by{|d| d.announce}.last}
+    
+  end
+  def daylies_of_a_day
+    @models = @Model.where(:date  => params[:date],:location =>  @weather_location).
+      order("announce")
+  end
+  def index_sub
+    @Pagenation = 3
+    @page = params[:page] || 1
+    @dayly = @Model.order("date desc").group(:month).paginate( :page =>  @page,:per_page => @Pagenation)
+    #page_monthes = monthes[(@page-1)*@Pagenation,@Pagenation]
+    @models = @Model.where(:month  => @dayly.map(&:month)).
+      where(:location =>  @weather_location).
+      order("date,announce").
+      group_by{ |d| d.date }.
+      map{|date,daylies| daylies.sort_by{|d| d.announce}.last}.
+      group_by{ |dayly| dayly.month }.map{|m,d| d[0]}
+  end
+
 end
+__END__
+@Model=Forecast
+def params
+@params ||= {}
+end
+@weather_location="maebashi"
