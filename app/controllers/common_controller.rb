9e761b80dfd508a702500232af7c35e3ecff992e
Dezawa Shinichiro <dezawa@aliadne.net>
2015-05-26 08:04:34 +0900

    グラフ表示に file_path での指定を可能にした

--- app/controllers/common_controller.rb
+++ app/controllers/common_controller.rb
@@ -2,11 +2,16 @@ class CommonController < ApplicationController
   include Actions
   include Permittion
   
-   def show_img
-     graph_dir  = Rails.root+
-       (params[:graph_dir].blank? ? "tmp/img"  :  params[:graph_dir])
-     graph_file = graph_dir + (params[:graph_file].blank? ? "power" : params[:graph_file])
-     graph_format = params[:graph_format].blank? ? :gif : params[:graph_format]
-     send_file "#{graph_file}.#{graph_format}", :type => "#{graph_format}", :disposition => 'inline'
+  def show_img
+    graph_format = params[:graph_format].blank? ? :gif : params[:graph_format]
+    graph_path =
+      if params[:graph_path] ;params[:graph_path]
+      else
+        graph_dir  = Rails.root+
+          (params[:graph_dir].blank? ? "tmp/img"  :  params[:graph_dir])
+        graph_file = graph_dir + (params[:graph_file].blank? ? "power" : params[:graph_file])
+        "#{graph_file}.#{graph_format}" 
+      end
+     send_file graph_path, :type => "#{graph_format}", :disposition => 'inline'
    end
 end
