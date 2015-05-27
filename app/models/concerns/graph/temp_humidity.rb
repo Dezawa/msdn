72e0e72afb4e75fe5866b14b5b654f7903147981
Dezawa Shinichiro <dezawa@aliadne.net>
2015-05-28 07:27:25 +0900

    Gnuplot派生 Graph::TempHumidity にて  予報と実績を同じグラフに重ね書きするために、create_by_multi_models を作成。  これに使うために、#title_post_time_range をmethod抽出

--- app/models/concerns/graph/temp_humidity.rb
+++ app/models/concerns/graph/temp_humidity.rb
@@ -8,39 +8,66 @@ module Graph
       @option = Gnuplot::DefaultOptionST.merge(TempHumidityDefST).merge(opt) 
     end
 
-    def self.create(models,option=Gnuplot::OptionST.new)
+    def self.create_by_models(models,option=Gnuplot::OptionST.new)
+      title ,time_range = title_post_time_range(models,option)
+
       location=models.first.location
       class_name = models.first.class.name.underscore.sub(/\//,"_")
-        title ,time_range=
-          case models.size
-          when 1 ;
-            ["(#{models.first.date.str('%Y-%m-%d)')})",
-             Graph::Base::TimeRange[:dayly]
-            ]
-          else   ;
-            ["(#{models.first.date.str('%Y-%m-%d')} - #{models.last.date.str('%Y-%m-%d)')})",
-             Graph::Base::TimeRange[ if models.first.date.year < models.last.date.year ; :years
-                                     else ; :monthly
-                                     end
-                                   ]
-            ]
-          end          
-        option = 
-          Gnuplot::OptionST.
-          new({graph_file: "#{class_name}_#{location}"},
-              common: { point_type: [6,6,6],point_size: [0.5,0.3,0.5],
-                       title_post: title,
-                      with: ["lines","lines","lines",nil],
-                      }.merge(time_range)
-             )#.merge!(option)
-        #pp option
-        data_list =
-          models.map{|model|
-          #pp [:temp,:vaper,:humi].map{|sym| weather[sym]}
-          model.times.map{|t| t.str("%Y-%m-%d %H:%M")}.
-              zip( *[:temperature,:humidity,:vaper].map{|sym| model.send sym }  )
-        }.flatten(1).sort_by{|d| d[0]}
-        self.new( data_list,option )
-      end
+      option = 
+        Gnuplot::OptionST.
+        new({graph_file: "#{class_name}_#{location}"},
+            common: { point_type: [6,6,6],point_size: [0.5,0.3,0.5],
+                     title_post: title,
+                    }.merge(time_range)
+           ).merge(option)
+      #pp option
+      data_list =
+        models.map{|model|
+        #pp [:temp,:vaper,:humi].map{|sym| weather[sym]}
+        model.times.map{|t| t.str("%Y-%m-%d %H:%M")}.
+          zip( *[:temperature,:humidity,:vaper].map{|sym| model.send sym }  )
+      }.flatten(1).sort_by{|d| d[0]}
+      self.new( data_list,option )
+    end
+
+    def self.create_by_multi_models(array_of_models,option=Gnuplot::OptionST.new)
+      title ,time_range = title_post_time_range(array_of_models.flatten,option)
+
+      location=array_of_models.first.first.location
+      class_name = array_of_models.
+        map{|models| models.first.class.name.underscore.sub(/\//,"_")}.join("_")
+      option = 
+        Gnuplot::OptionST.
+        new({graph_file: "#{class_name}_#{location}"},
+            common: { point_type: [6,6,6],point_size: [0.5,0.3,0.5],
+                     title_post: title,
+                    }.merge(time_range)
+           ).merge(option)
+      #pp option
+      data_list =  array_of_models.map.with_index{|models,idx|
+        [%w(予報 実測)[idx],
+         models.map{|model|
+           model.times.map{|t| t.str("%Y-%m-%d %H:%M")}.
+             zip( *[:temperature,:humidity,:vaper].map{|sym| model.send sym }  )
+         }.flatten(1).sort_by{|d| d[0]}
+        ]
+      }
+      self.new( data_list,option )
+    end
+
+    def self.title_post_time_range(models,option)
+      time_range =
+        if option[:header][:time_range] ;   option[:header].delete(:time_range)
+        elsif models.size ==1            ; :dayly
+        elsif models.first.date.year < models.last.date.year ; :years
+        else                             ; :monthly
+        end
+      
+      title_post = "(#{models.first.date.str('%Y-%m-%d)')}" +
+        if models.size > 1 ;" - #{models.last.date.str('%Y-%m-%d)')})"
+        else               ;")"
+        end
+      [ title_post, Graph::Base::TimeRange[time_range] ]
+    end          
   end
 end
