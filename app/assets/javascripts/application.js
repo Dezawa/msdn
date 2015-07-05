89b84d7f2d8498e8c357d707f405dd09654fcbbd
Dezawa Shinichiro <dezawa@aliadne.net>
2015-07-05 16:30:27 +0900

    POPUPにjQuery利用に変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,82 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+// ######### POPUP ########
+// height:600, // sets the height in pixels of the window.
+// width:600, // sets the width in pixels of the window.
+// toolbar:0, // determines whether a toolbar (includes the forward and back buttons) is displayed {1 (YES) or 0 (NO)}.
+// scrollbars:0, // determines whether scrollbars appear on the window {1 (YES) or 0 (NO)}.
+// status:0, // whether a status line appears at the bottom of the window {1 (YES) or 0 (NO)}.
+// resizable:1, // whether the window can be resized {1 (YES) or 0 (NO)}. Can also be overloaded using resizable.
+// left:0, // left position when the window appears.
+// top:0, // top position when the window appears.
+// center:0, // should we center the window? {1 (YES) or 0 (NO)}. overrides top and left
+// createnew:1, // should we create a new window for each occurance {1 (YES) or 0 (NO)}.
+// location:0, // determines whether the address bar is displayed {1 (YES) or 0 (NO)}.
+// menubar:0, // determines whether the menu bar is displayed {1 (YES) or 0 (NO)}.
+// onUnload:null // function to call when the window is closed
+var profiles =
+{
+
+	window68:
+	{
+		height:600,
+	    width:800,
+            scrollbars:1,
+		status:1
+	},
+	window88:
+	{
+		height:800,
+		width:800,
+            scrollbars:1,
+		status:1
+	},
+
+	window22:
+	{
+		height:200,
+		width:200,
+		status:1,
+		resizable:0
+	},
+
+	windowCenter:
+	{
+		height:300,
+		width:400,
+		center:1
+	},
+
+	windowNotNew:
+	{
+		height:300,
+		width:400,
+		center:1,
+		createnew:0
+	},
+
+	windowCallUnload:
+	{
+		height:300,
+		width:400,
+		center:1,
+		onUnload:unloadcallback
+	},
+
+};
+
+function unloadcallback(){
+	alert("unloaded");
+};
+
+
+$(function()
+{
+  		$(".popupwindow").popupwindow(profiles);
+});
+
 $(function(){
 		$('#year_year').change(function(){
 		var the_year = $("#year_year").val();
--- app/controllers/sola/controller.rb
+++ app/controllers/sola/controller.rb
@@ -7,8 +7,8 @@ class Sola::Controller < CommonController
      Menu.new(   "日々ピーク",:dayly   ,:action => :index) ,
      Menu.new(   "日々発電量",:dayly   ,:action => :index_day_total) ,
      Menu.new(   "モニタデータ",:dayly ,:action => :index_monitor) ,
-     Menu.new(   "ピークグラフ",:dayly ,{ :action => :peak_graph}  ,:win_name => "newtab", :target =>"newtab") ,
-     Menu.new(   "相関"  ,:dayly ,{ :action => :correlation},:target =>"newtab") ,
+     Menu.new(   "ピークグラフ",:dayly ,{ :action => :peak_graph}  ,class: "popupwindow",rel: "window68" ) ,
+     Menu.new(   "相関"  ,:dayly ,{ :action => :correlation} ,class: "popupwindow",rel: "window68") ,
      Menu.new(   "電池残量"  ,"status/tand_d" ,{ :action => :list}) ,
     ]
 
